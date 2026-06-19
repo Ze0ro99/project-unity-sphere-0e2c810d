6d38710f4341d9063fa5a924605c4065744e5848
Ze0ro99 <kamelkadah910@gmail.com>
2026-06-19 16:12:47 +0300

    Sovereign Branch Sync (feature/pirc-v5-1-adaptive-engine): Secured Cloud Authentication Matrix (2026-06-19 15:52:50) [Skip CI]

--- contracts/soroban/target/release/build/num-traits-2a8f1e2f77fb0cb0/build_script_build-2a8f1e2f77fb0cb0.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/build/num-traits-2a8f1e2f77fb0cb0/build_script_build-2a8f1e2f77fb0cb0.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.17/build.rs
-
-/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/build/num-traits-2a8f1e2f77fb0cb0/build_script_build-2a8f1e2f77fb0cb0: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.17/build.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.17/build.rs:
--- contracts/soroban/target/wasm32-unknown-unknown/release/deps/soroban_env_guest-5628887e56f35e85.d
+++
@@ -1,8 +0,0 @@
-/home/runner/work/PiRC/PiRC/contracts/soroban/target/wasm32-unknown-unknown/release/deps/soroban_env_guest-5628887e56f35e85.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/guest.rs
-
-/home/runner/work/PiRC/PiRC/contracts/soroban/target/wasm32-unknown-unknown/release/deps/libsoroban_env_guest-5628887e56f35e85.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/guest.rs
-
-/home/runner/work/PiRC/PiRC/contracts/soroban/target/wasm32-unknown-unknown/release/deps/libsoroban_env_guest-5628887e56f35e85.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/guest.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-env-guest-20.3.0/src/guest.rs:
