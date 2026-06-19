79ed61143bec407c59b50ce8ec4a99f93265576a
Ze0ro99 <kamelkadah910@gmail.com>
2026-06-19 16:12:23 +0300

    Sovereign Branch Sync (feature/pirc-103-200-defi-core): Secured Cloud Authentication Matrix (2026-06-19 15:52:50) [Skip CI]

---
+++ contracts/soroban/target/release/build/stellar-strkey-75d5299ef57a313a/build_script_build-75d5299ef57a313a.d
@@ -0,0 +1,5 @@
+/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/build/stellar-strkey-75d5299ef57a313a/build_script_build-75d5299ef57a313a.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stellar-strkey-0.0.8/build.rs
+
+/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/build/stellar-strkey-75d5299ef57a313a/build_script_build-75d5299ef57a313a: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stellar-strkey-0.0.8/build.rs
+
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stellar-strkey-0.0.8/build.rs:
---
+++ contracts/soroban/target/release/deps/num_integer-89c71e4d9bf55992.d
@@ -0,0 +1,9 @@
+/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/deps/num_integer-89c71e4d9bf55992.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/roots.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/average.rs
+
+/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/deps/libnum_integer-89c71e4d9bf55992.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/roots.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/average.rs
+
+/home/runner/work/PiRC/PiRC/contracts/soroban/target/release/deps/libnum_integer-89c71e4d9bf55992.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/roots.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/average.rs
+
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/lib.rs:
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/roots.rs:
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.45/src/average.rs:
