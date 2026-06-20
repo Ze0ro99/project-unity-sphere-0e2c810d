7881bdc10a35bc99400b8bfe740f0d021eaf46e4
Ze0ro99 <kamelkadah910@gmail.com>
2026-06-20 21:05:35 +0300

    build(sync): Sovereign Matrix Deployment - Branch: v2 - Integrity: 4cbfce626fd6d27b38db7f5b9752dd0b78b7c38501ec157ac4e2690921addb84

--- packages/economics/src/config/api.ts
+++ packages/economics/src/config/api.ts
@@ -3,7 +3,7 @@
  * Target: Lovable Workspace & Pi Network Blockchain Parameters
  */
 export const SOVEREIGN_NETWORK_CONFIG = {
-    synchronizedAtTimestamp: "2026-06-20T16:45:14Z",
+    synchronizedAtTimestamp: "2026-06-20T17:28:30Z",
     activeTrackingBranch: "v2",
     payloadIntegrityHash: "SHA256:4cbfce626fd6d27b38db7f5b9752dd0b78b7c38501ec157ac4e2690921addb84",
     invariantRule: "10_000_000_micro_to_1_pi",
